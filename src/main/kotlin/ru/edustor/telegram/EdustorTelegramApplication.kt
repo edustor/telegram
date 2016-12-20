1a90a93447cfd7104f25436ff30b6a0d856ee35f
Dmitry Romanov <me@wutiarn.ru>
2016-12-20 15:12:39 +0300

    Spring boot app init

---
+++ src/main/kotlin/ru/edustor/telegram/EdustorTelegramApplication.kt
@@ -0,0 +1,10 @@
+package ru.edustor.telegram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+@Configuration
+open class EdustorTelegramApplication
---
+++ src/main/kotlin/ru/edustor/telegram/main.kt
@@ -0,0 +1,7 @@
+package ru.edustor.telegram
+
+import org.springframework.boot.SpringApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EdustorTelegramApplication::class.java, *args)
+}
